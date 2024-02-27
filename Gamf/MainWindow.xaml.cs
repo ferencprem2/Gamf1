6a9c588bf81a9fcc94063b413edeb560ff5d5ee4
prem.ferenc <prem.ferenc@CAD2-02>
2024-02-27 14:40:01 +0100

    15ms

--- Gamf/MainWindow.xaml.cs
+++ Gamf/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,77 @@ namespace Gamf
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<long> numberList = new();
         public MainWindow()
         {
+            
             InitializeComponent();
+            ReadNumberTxtFile();
+            
+        }
+
+        public void ReadNumberTxtFile()
+        {
+            StreamReader sr = new("szamok.txt");
+            while (!sr.EndOfStream)
+            {
+                numberList.Add(long.Parse(sr.ReadLine()));
+            //return numberList.Count(x => IsRealativePrimeNumbers(x, 1310438493) == 1);
+            }
+        }
+
+        //public int CountRelativePrimes()
+        //{
+        //    int count = 0;
+        //    foreach (long number in numberList)
+        //    {
+        //        if (IsRealativePrimeNumbers(number, 1310438493) == 1)
+        //        {
+        //            count++;
+        //        }
+        //    }
+        //    return count;
+        //}
+
+        //public long IsRealativePrimeNumbers(long firstNumber, long secondNumber)
+        //{
+        //    while(secondNumber != 0)
+        //    {
+        //        long temp = secondNumber;
+        //        secondNumber = firstNumber % secondNumber;
+        //        firstNumber = temp;
+        //    }
+        //    return firstNumber;
+        //}
+
+        public int CountRelativePrimes()
+        {
+            int count = 0;
+            foreach (long number in numberList)
+            {
+                long secondNumber = 1310438493;
+                long firstNumber = number;
+                while (secondNumber != 0)
+                {
+                    long temp = secondNumber;
+                    secondNumber = firstNumber % secondNumber;
+                    firstNumber = temp;
+                    if (firstNumber == 1)
+                    {
+                        count++;
+                    }
+                }
+                
+            }
+            return count;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var ora = Stopwatch.StartNew();
+            firstLabel.Content = $"{CountRelativePrimes()}";
+            ora.Stop();
+            timeLabel.Content = $"{ora.Elapsed}";
         }
     }
 }
